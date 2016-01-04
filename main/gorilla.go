364318560125ab6d5678b8eb1cfffc7d1b87b3d0
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-01-04 23:28:27 +0900

    ゴリラを出力してみる

---
+++ main/gorilla.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("　　　　　　　　　,r\"´⌒｀ﾞ`ヽ")
+    fmt.Println("　　　　　　　／　,　　　-‐- !､")
+    fmt.Println("　　　 　　／　{,}f　　-‐- ,,,__､)")
+    fmt.Println("　　　　／　　 /　　.r'~\"''‐--､)")
+    fmt.Println("　　,r''\"´⌒ヽ{　　 ヽ　(・)ﾊ(・)}､")
+    fmt.Println("　/　　　　　　＼　　（⊂｀-'つ）i-､")
+    fmt.Println("　　　　　　　　　 `}. （__,,ノヽ_ﾉ,ﾉ　 ＼")
+    fmt.Println("　　　　　　　　　　 l　　 ｀-\" ,ﾉ　　　 ヽ")
+    fmt.Println("　　　　　　　　　　 }　､､___,j''　　　　　 l\")")
+}
