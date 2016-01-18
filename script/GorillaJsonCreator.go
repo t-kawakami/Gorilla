a887c4838dcc837b55a62afeee99dbfc3082b379
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-01-18 23:49:26 +0900

    日付け型のJSONフォーマッタ

--- script/GorillaJsonCreator.go
+++ script/GorillaJsonCreator.go
@@ -5,15 +5,30 @@ import (
 	"time"
 )
 
+type jsonTime struct {
+	time.Time
+}
+
+// formatを指定する。2006-01-02や2006-01-02 03:04:05など。
+func (jsonTime jsonTime) format() string {
+	return jsonTime.Time.Format("2006-01-02")
+}
+
+func (jsonTime jsonTime) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + jsonTime.format() + `"`), nil
+}
+
+
 type Gorilla struct {
 	Name string `json:"name"`
 	Age int `json:age`
 	Children[] Gorilla `json:children`
-	BirthDay time.Time `json:birthday`
+	BirthDay jsonTime `json:birthday`
 }
 
 func main() {
-	gorilla := Gorilla{Name:"Boss", Age:16, Children:[]Gorilla{}, BirthDay:time.Now()}
+	loc, _ := time.LoadLocation("Asia/Tokyo")
+	gorilla := Gorilla{Name:"Boss", Age:16, Children:[]Gorilla{}, BirthDay:jsonTime{time.Date(2015,1,18,0,0,0,0,loc)}}
 	jsonString, _ := json.MarshalIndent(gorilla, " ", "    ")
 	fmt.Printf(string(jsonString))
 }
